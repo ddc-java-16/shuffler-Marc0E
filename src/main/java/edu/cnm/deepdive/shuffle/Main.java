84686c4cedc64a9bed875de21b6aa9687fffb638
Marc0E <mestradamonge@cnm.edu>
2023-09-20 15:35:57 -0600

    take data to shuffle from comman line

--- src/main/java/edu/cnm/deepdive/shuffle/Main.java
+++ src/main/java/edu/cnm/deepdive/shuffle/Main.java
@@ -5,10 +5,18 @@ import java.util.Arrays;
 public class Main {
 
   public static void main(String[] args) {
-    int [] data = new int[100];
-    for (int i = 0; i < data.length; i++){
-      data[i] = 100 + i;
+
+    int [] data = new int[args.length];
+    int i = 0;
+    try {
+      for (i =0;i< args.length;i++){
+        data[i] = Integer.parseInt(args[i]);
+      }
+    } catch (NumberFormatException e) {
+      //System.out.println(e.getMessage());other option down
+      System.err.printf("Parsing failed %s%n", e.getMessage());
     }
+    data = Arrays.copyOf(data, i);
     System.out.println(Arrays.toString(data));
     Shuffler shuffler = new Shuffler();
     shuffler.shuffle(data);
